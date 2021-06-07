a13093de935d1272edee2cafea5bd9c6309de244
Roberta <robyrubirex@gmail.com>
2021-06-06 22:33:10 -0300

    testear clientes

--- app/models/Usuario.php
+++ app/models/Usuario.php
@@ -69,7 +69,7 @@ class Usuario extends Sector
 
         for($i=0;$i<count($lista);$i++)
         {
-            echo $lista[$i]->id
+            echo $lista[$i]->id;
             if($lista[$i]->id==$id&&(strcmp($lista[$i]->tipo,"mozo"))==0)
             {
                 return True;
